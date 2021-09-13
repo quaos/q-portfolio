0f9d44d88138c90ac8c41849f3c49f4877178de0
Chakrit W <quaos.qrz@gmail.com>
2021-09-13 09:08:53 +0700

    Add deps fix for react-router

--- src/deps/react-dom.ts
+++ src/deps/react-dom.ts
@@ -1,2 +1,2 @@
-export { default as ReactDOM } from "https://esm.sh/react-dom@17.0.2?dev&no-check";
+export { default as ReactDOM } from "https://esm.sh/react-dom@17.0.2?deps=react@17.0.2&dev&no-check";
 //"https://esm.sh/[react@17.0.1,react-dom@17.0.1,react-router-dom@5.2.0]/react-dom?dev&no-check";
--- src/deps/react-router.ts
+++ src/deps/react-router.ts
@@ -7,5 +7,5 @@ export {
   useLocation,
   useParams,
   useRouteMatch,
-} from "https://esm.sh/react-router-dom@5.2.1?dev&no-check";
+} from "https://esm.sh/react-router-dom@5.2.1?deps=react@17.0.2&dev&no-check";
 //"https://esm.sh/[react@17.0.1,react-dom@17.0.1,react-router-dom@5.2.0]/react-router-dom?dev&no-check";
